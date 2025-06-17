4e632fc994852404a16862c856f7d38733f82e8d
naqvihassan <168823161+naqvihassan@users.noreply.github.com>
2025-06-18 01:21:34 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,7 @@
-/* PROGRAMMING FUNDAMENTAL'S PROJECT FOR FALL 2022 BS(CS)
- * You don't need to change anything in the driver code/main function except the part(s) where instructed.
- * You are expected to define your functionalities in the "functionality.h" header and call them here in the instructed part(s).
- * The game is started with a small box, you need to create other possible in "pieces.h" file in form of array.
-    E.g., Syntax for 2 pieces, each having four coordinates -> int Pieces[2][4];
- * Load the images into the textures objects from img directory.
- * Don't make any changes to the "utils.h" header file except for changing the coordinate of screen or global variables of the game.
- * SUGGESTION: If you want to make changes in "utils.h", do it once you complete rest of the functionalities.
- * TIP: You can study SFML for drawing scenes, sprites and window.
- * */
+
 
 
 //----HASSAN NAQVI---//
-//----22i-0797_C----//
 #include <fstream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
